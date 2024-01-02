242c5081697b3cfcd526b37073463d4e7f1f9fdb
lukas <lukas.arnhold@hotmail.com>
2024-01-02 23:03:14 +0100

    persist triples

--- core/src/main/kotlin/org/arnhold/evaluator/tripleStore/TripleStoreServiceImpl.kt
+++ core/src/main/kotlin/org/arnhold/evaluator/tripleStore/TripleStoreServiceImpl.kt
@@ -1,12 +1,19 @@
 package org.arnhold.evaluator.tripleStore
 
-import org.apache.jena.query.Dataset
+import org.apache.jena.ontology.OntModel
 import org.apache.jena.rdf.model.Model
-import org.apache.jena.tdb.TDBFactory
+import org.apache.jena.rdf.model.ModelFactory
+import org.apache.jena.riot.Lang
+import org.apache.jena.riot.RDFDataMgr
 import org.arnhold.evaluator.configuration.TripleStoreConfig
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import java.util.UUID
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.nio.file.Path
+import java.util.*
 
 
 @Component
@@ -14,30 +21,31 @@ class TripleStoreServiceImpl @Autowired constructor(
     val tripleStoreConfig: TripleStoreConfig
 ) : TripleStoreService {
 
-    final var store: Dataset
-
-    init {
-        val directory = String.format("%s", tripleStoreConfig.directory)
-        store = TDBFactory.createDataset(directory)
-    }
-
     override fun saveModel(id: UUID, model: Model) {
-        store.addNamedModel(id.toString(), model)
+        try {
+            FileOutputStream(Path.of(tripleStoreConfig.directory, String.format("%s.ttl", id.toString())).toFile(), false).use { fileWriter ->
+                RDFDataMgr.write(fileWriter, model, Lang.TURTLE)
+            }
+        } catch (e: IOException) {
+            throw e
+        }
     }
 
     override fun getModel(id: UUID): Model {
-        return store.getNamedModel(id.toString())
+        val model: Model = ModelFactory.createDefaultModel()
+        val inputStream: InputStream = FileInputStream(Path.of(tripleStoreConfig.directory, String.format("%s.ttl", id.toString())).toFile())
+        return model.read(Path.of(tripleStoreConfig.directory, String.format("%s.ttl", id.toString())).toFile().absolutePath)
     }
 
     override fun updateModel(id: UUID, model: Model) {
-        store.replaceNamedModel(id.toString(), model)
+        saveModel(id, model)
     }
 
     override fun getAllModels(): List<UUID> {
-        return store.listNames().asSequence().map { UUID.fromString(it) }.toList()
+        return listOf()
     }
 
     override fun removeModel(id: UUID) {
-        store.removeNamedModel(id.toString())
+        // TODO
     }
 }
