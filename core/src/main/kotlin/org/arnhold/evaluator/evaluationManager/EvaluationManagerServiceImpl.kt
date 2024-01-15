9e37005bbc699e1d7cec3039f111a3ad05dbbd3e
Lukas Arnhold <lukas.arnhold@hotmail.com>
2024-01-15 23:38:30 +0100

    save measurements individually

--- core/src/main/kotlin/org/arnhold/evaluator/evaluationManager/EvaluationManagerServiceImpl.kt
+++ core/src/main/kotlin/org/arnhold/evaluator/evaluationManager/EvaluationManagerServiceImpl.kt
@@ -1,6 +1,8 @@
 package org.arnhold.evaluator.evaluationManager
 
 import mu.KotlinLogging
+import org.apache.jena.ontology.OntModel
+import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.reasoner.Reasoner
 import org.apache.jena.reasoner.ReasonerRegistry
@@ -9,7 +11,7 @@ import org.arnhold.evaluator.evaluationProvider.EvaluationProviderService
 import org.arnhold.evaluator.metricProcessing.MetricProcessingService
 import org.arnhold.sdk.common.dqv.Category
 import org.arnhold.sdk.common.dqv.Dimension
-import org.arnhold.sdk.common.ontologyDefinitions.DMPDQV
+import org.arnhold.sdk.common.dqv.Measurement
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.util.UUID
@@ -27,18 +29,16 @@ class EvaluationManagerServiceImpl @Autowired constructor(
 
         val contextDMPId = dataProviderService.loadContextualizedDMP(parameters.dmpLoaderParameters)
         val contextDMP = dataProviderService.getContextualizedDMP(contextDMPId)
-
-        contextDMP.setNsPrefix("dmpdqv", DMPDQV.URI_PREFIX)
-        contextDMP.setNsPrefix("dqv", DMPDQV.DQV_PREFIX)
-        contextDMP.add(dataProviderService.getDMPDQVOntology())
+        
         val measurements = metricProcessingService.produceAllMeasurements(contextDMP, parameters.dataLifecycle)
+        val measurementsModel = measurementsToModel(dataProviderService.getDMPDQVOntology(), measurements)
 
         //After adding measurements to contextDMP save and run reasoner over dmpdqv to get correct rdf types from predicate relations
-        logger.info { "Reason over DMP with measurements" }
+        logger.info { "Reason over measurements" }
         val reasoner: Reasoner = ReasonerRegistry.getOWLReasoner()
         reasoner.bindSchema(dataProviderService.getDMPDQVOntology())
-        val reasonedModel = ModelFactory.createInfModel(reasoner, contextDMP)
-        dataProviderService.saveModel(reasonedModel)
+        val reasonedMeasurementsModel = ModelFactory.createInfModel(reasoner, measurementsModel)
+        dataProviderService.saveModel(reasonedMeasurementsModel)
 
         logger.info { "Return evaluation results" }
         return EvaluationTaskResult(
@@ -51,5 +51,12 @@ class EvaluationManagerServiceImpl @Autowired constructor(
 
     override fun getEvaluatorInformation(): Map<Category, List<Dimension>> {
         return evaluationProviderService.getAllEvaluators().map { it.getPluginInformation() }.groupBy ( {it.belongsToCategory}, {it.applicableDimension} )
+    }
+
+    private fun measurementsToModel(dmpdqv: OntModel, measurements: List<Measurement>): Model {
+        logger.info { "Integrate measurements into semantic DMP model" }
+        val model = dmpdqv.baseModel
+        measurements.mapIndexed {index, measurement -> measurement.toResource(dmpdqv, "Measurement_$index")}
+        return model
     }
 }
--- core/src/main/kotlin/org/arnhold/evaluator/metricProcessing/MetricProcessingServiceImpl.kt
+++ core/src/main/kotlin/org/arnhold/evaluator/metricProcessing/MetricProcessingServiceImpl.kt
@@ -6,23 +6,15 @@ import org.arnhold.evaluator.evaluationProvider.EvaluationProviderService
 import org.arnhold.sdk.common.constants.DataLifecycle
 import org.arnhold.sdk.common.dqv.Dimension
 import org.arnhold.sdk.common.dqv.Measurement
-import org.arnhold.sdk.common.ontologyDefinitions.DMPDQV
-import org.arnhold.sdk.tools.SparqlSelector
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import java.nio.file.Path
 
 @Component
 class MetricProcessingServiceImpl @Autowired constructor(
-    val evaluationProviderService: EvaluationProviderService,
-    val sparqlSelector: SparqlSelector,
+    val evaluationProviderService: EvaluationProviderService
 ) : MetricProcessingService {
 
     private val logger = KotlinLogging.logger {}
-
-    companion object {
-        const val SPARQL_DIRECTORY = "data/selectors/"
-    }
 
     override fun produceMeasurementsForDimension(dmp: Model, dimension: Dimension): List<Measurement> {
         TODO("Not yet implemented")
@@ -30,22 +22,6 @@ class MetricProcessingServiceImpl @Autowired constructor(
 
     override fun produceAllMeasurements(dmp: Model, lifecycle: DataLifecycle): List<Measurement> {
         logger.info { "Produce all available metrics for lifecycle $lifecycle" }
-        val measurements = evaluationProviderService.getAllEvaluators().flatMap { it.getAllMeasurements(dmp, lifecycle) }
-        addToDmp(dmp, measurements)
-        return measurements
-    }
-
-    private fun addToDmp(dmp: Model, measurements: List<Measurement>) {
-        logger.info { "Integrate measurements into semantic DMP model" }
-        val measurementResources = measurements.mapIndexed {index, measurement -> measurement.toResource(dmp, "Measurement_$index")}
-        val query = Path.of(SPARQL_DIRECTORY + "dmps.sparql").toFile().readText(Charsets.UTF_8)
-        val selected = sparqlSelector.getSelectResults(dmp, query)
-        selected.forEach { dmpResource ->
-            val dmpRoot = dmpResource.resources.get("subject")
-            measurementResources.forEach {measurementResource ->
-                logger.info { "Add measurement ${measurementResource.uri}" }
-                dmpRoot?.addProperty(DMPDQV.HAS_QUALITY_MEASUREMENT, measurementResource)
-            }
-        }
+        return evaluationProviderService.getAllEvaluators().flatMap { it.getAllMeasurements(dmp, lifecycle) }
     }
 }
