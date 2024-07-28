234dd5f6c16f427a9e5bb2376657ff2e84b00502
Lukas <lukas.arnhold@hotmail.com>
2024-07-28 23:08:13 +0200

    load context to accuracy evaluator

--- case-study/src/main/kotlin/org/arnhold/dmpeval/casestudy/evaluation/feasibilityCategoryEvaluators/AccuracyDimensionEvaluator.kt
+++ case-study/src/main/kotlin/org/arnhold/dmpeval/casestudy/evaluation/feasibilityCategoryEvaluators/AccuracyDimensionEvaluator.kt
@@ -1,19 +1,28 @@
 package org.arnhold.dmpeval.casestudy.evaluation.feasibilityCategoryEvaluators
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import mu.KotlinLogging
 import org.apache.jena.ontology.OntModel
 import org.apache.jena.rdf.model.Model
 import org.arnhold.dmpeval.casestudy.evaluation.CategoryDimmensionModels
+import org.arnhold.dmpeval.casestudy.evaluation.qualityOfActionsCategoryEvaluators.model.fuji.FujiRoot
+import org.arnhold.sdk.context.schema.Dataset
 import org.arnhold.sdk.model.EvaluationDimensionConstants
 import org.arnhold.sdk.vocab.dqv.Measurement
 import org.arnhold.sdk.evaluator.DimensionEvaluatorPlugin
 import org.arnhold.sdk.evaluator.EvaluatorInformation
 import org.arnhold.sdk.model.EvaluationTaskParameters
+import org.arnhold.sdk.vocab.constants.ContextSchema
 import org.arnhold.sdk.vocab.constants.Extension
 import org.arnhold.sdk.vocab.context.DMPContext
+import org.re3data.schema._2_2.Re3Data.Repository
 import org.springframework.stereotype.Component
 
 @Component
 class AccuracyDimensionEvaluator : DimensionEvaluatorPlugin {
+
+    private val logger = KotlinLogging.logger {}
 
     override fun getPluginIdentifier(): String {
         return EvaluationDimensionConstants.ACCURACY.toString()
@@ -34,6 +43,15 @@ class AccuracyDimensionEvaluator : DimensionEvaluatorPlugin {
         dmpOntology: OntModel,
         extensionOntologies: Map<Extension, OntModel>
     ): List<Measurement> {
+        logger.info { "Get all accuracy measurements" }
+        val reader = ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+        val re3dataContext = context.find { it.vocabularyIdentifier === ContextSchema.HOST }?.value
+        val openAireContext = context.find { it.vocabularyIdentifier === ContextSchema.DATASET }?.value
+
+        val re3DataHost = reader.readValue(re3dataContext, Repository::class.java)
+        val openAireDataset = reader.readValue(openAireContext, Dataset::class.java)
+
         return listOf()
     }
 
