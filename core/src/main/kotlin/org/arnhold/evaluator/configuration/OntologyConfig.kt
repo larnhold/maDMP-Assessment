db466c128164cd58789bcc466fefc882bdcd9737
Lukas <lukas.arnhold@hotmail.com>
2024-08-01 13:39:47 +0200

    remove unused code

--- core/src/main/kotlin/org/arnhold/evaluator/configuration/OntologyConfig.kt
+++ core/src/main/kotlin/org/arnhold/evaluator/configuration/OntologyConfig.kt
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "ontology")
 data class OntologyConfig(
-    var DCSLocation: String = "./data/ontologies/dcso-4.0.1.ttl",
-    var DMPDQVLocation: String = "./data/ontologies/dmpqv.ttl"
+    var DCSLocation: String = "",
+    var DMPDQVLocation: String = ""
 )
